73fb08485e284d8d71f127dcb274ab59ac5f9836
Ruthael <110993814+Ruthael@users.noreply.github.com>
2022-10-12 23:53:36 +0300

    Update main.h

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,19 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+#include <stddef.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-/**
- * enum bool - typedef function for bool
- * @TRUE: 1
- * @FALSE: 0
-*/
-typedef enum bool
-{
-	TRUE = 1,
-	FALSE = 0
-} bool;
-
-#endif /* FUNCTION_POINTERS_H */
+#endif
